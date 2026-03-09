3c10083608a72b146aa34b3cc4140ba04cb97704
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2026-03-09 22:57:51 +0100

    Removes @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import { promises as fs } from 'fs';
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({
@@ -27,7 +26,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans`}>
         {children}
-        <Analytics />
       </body>
     </html>
   );
